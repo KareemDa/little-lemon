888d89cd9b9a757844be9d0f8521c7eb4a923e9a
Kareem Dabbeet <kareemda1@gmail.com>
2023-05-12 14:52:02 +0300

    initial commit'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import BaseHeader from './components/BaseHeader'
+import './App.css'
+import Landing from './components/Landing'
+import Menus from './components/Menus'
+import Contact from './components/Cantact'
+import BaseFooter from './components/BaseFooter'
+import Specials from './components/Specials'
+import Reviews from './components/Reviews'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<BaseHeader />
+
+			<Landing />
+
+			<Menus />
+
+			<Specials />
+
+			<Reviews />
+
+			<Contact />
+
+			<BaseFooter />
+		</div>
+	)
 }
 
-export default App;
+export default App
